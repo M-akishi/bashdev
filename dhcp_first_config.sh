bb6d92bdfcccfe1b7c104c38003b2838be7431c7
Maxi <76972484+M-akishi@users.noreply.github.com>
2023-11-28 17:55:09 -0300

    main

---
+++ dhcp_first_config.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+archive="/etc/dhcp/dhcpd.conf"
+
+preconf="ddns-update-style interim;\ndefault-leade-time 600;\nmax-lease-time 7200;"
+
+read -p "Ingrese la ip de la red : " subnet
+read -p "Ingrese la mascara de red : " netmask
+read -p "Ingrese la ip del router : " router
+read -p "Rango de la red? : " range
+
+text="\nsubnet $subnet netmask $netmask {\n\toption subnet-mask\t$netmask;\n\toption routers\
+\t$router;\n\trange\t$range;\n\toption domain-name-servers\t$router}}"
+
+if [ -e "$archive" ]; then
+    echo -e "$preconf" >> "$archive"
+    echo -e "$text" >> "$archive"
+else
+    echo "archivo de configuracion no existente, ha instalado el servidor?"
+fi
+
