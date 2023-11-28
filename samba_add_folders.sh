2842bf70548dee265320d0e577f1a6d37c39327e
Maxi <76972484+M-akishi@users.noreply.github.com>
2023-11-28 19:09:51 -0300

    fixes

--- samba_add_folders.sh
+++ samba_add_folders.sh
@@ -5,7 +5,7 @@ archive="/etc/samba/smb.conf"
 read -p "ingrese el nombre de la carpeta : " namefolder
 read -p "algun comentario para la carpeta? : " comment
 read -p "ubicacion de la carpeta desde la raiz : " path
-read -p "grupo de los que pueden acceder a la carpeta" group
+read -p "grupo de los que pueden acceder a la carpeta : " group
 
 folder="
 [$namefolder]
