1cf9b516314664f062599bb80ef6bc0687f49f02
Maxi <76972484+M-akishi@users.noreply.github.com>
2023-11-30 17:58:35 -0300

    testing

--- dhcp_full_config.sh
+++ dhcp_full_config.sh
@@ -70,7 +70,7 @@ dhcp_add_subnet(){
 }
 
 # Verificar si el servicio DHCP está instalado
-if command -v systemctl &> /dev/null && systemctl list-units --type=service --all | grep 'dhcpd'; then
+if [ "$(command -v systemctl &> /dev/null && systemctl list-units --type=service --all | grep 'dhcpd' | awk '{print $2}')" = "loaded" ]; then
     echo "El servidor DHCP (dhcpd) está instalado."
 
     # Verificar si el servicio DHCP está en ejecución
